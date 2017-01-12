eb3ef43a5b563eb0698735279d90a594edcff59d
Sakol Assawasagool <koobitor@gmail.com>
2017-01-13 01:34:31 +0700

    config grunt complie

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -46,10 +46,14 @@ module.exports = {
         name: 'Magento/candy',
         locale: 'th_TH',
         files: [
-            'css/styles-m',
-            'css/styles-l',
-            'css/email',
-            'css/email-inline'
+            'app/design/frontend/Magento/blank/web/css/styles-m',
+            'app/design/frontend/Magento/blank/web/css/styles-l',
+            'app/design/frontend/Magento/blank/web/css/email',
+            'app/design/frontend/Magento/blank/web/css/email-inline',
+            'app/design/frontend/Magento/Candy/web/css/styles-m',
+            'app/design/frontend/Magento/Candy/web/css/styles-l',
+            'app/design/frontend/Magento/Candy/web/css/email',
+            'app/design/frontend/Magento/Candy/web/css/email-inline',
         ],
         dsl: 'less'
     },
