261b5aa1b81e0bddd11a25dc7f377f8111c1516c
Sakol Assawasagool <koobitor@gmail.com>
2017-01-12 23:15:24 +0700

    change config theme

--- .modman/theme-candy/candy/registration.php
+++ .modman/theme-candy/candy/registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Magento/luma',
+    'frontend/Magento/candy',
     __DIR__
 );
